0f0286d3165e01ae53c0eeae61e4a01fa04ab171
hitesh-n <83066442+hitesh-n@users.noreply.github.com>
2022-06-11 21:48:43 +0530

    Update change_filename.py

--- Codes/change_filename.py
+++ Codes/change_filename.py
@@ -12,8 +12,8 @@ for filename in os.listdir(directory):
 for i in names:
 	if "xml" in i: 
 		with open(i) as file: data = file.readlines()
-    # modifies the location of the image file to match existing machine
-		data[3] = "<path>./drive/MyDrive/trial/train/" + i[:-3] + "jpg</path>" 
+    		# modifies the location of the image file to match existing machine
+		data[3] = "<path>./drive/MyDrive/trial/train/" + i.split('/')[-1][:-3] + "jpg</path>" 
 		with open(i, "w") as file: file.writelines(data)
 	
 print(names)
